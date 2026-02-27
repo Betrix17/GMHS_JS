96934291cb541a1424f0cfd78aa7f883b968921b
Betrix17 <beatamincic@gmail.com>
2026-02-27 10:23:55 +0100

    Update - doplnila jsem si lekci

---
+++ script.js
@@ -0,0 +1,36 @@
+alert("Zkouška")
+alert(3)
+
+let username = "Adélka";
+console.log(username);
+
+username = "Honza";
+console.log(username);
+
+console.log(7+4)
+console.log("7"+"4");
+console.log("7"+ 4);
+
+let age = 17;
+console.log(typeof text)
+let hodnota = null;
+
+let text = "Text";
+console.log (cisloA + cisloB)
+
+//Umocňování **
+console.assert.log(cisloA ** cisloB)
+//Zbytek
+console.log(cisloA % cisloB)
+
+//Aktualizace proměnné
+let number = 18;
+number = number + 1;
+
+number+=1;
+number-=2;
+number+=4;
+number/=6;
+number%=3;
+number++;
+number--;
