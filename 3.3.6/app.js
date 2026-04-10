e227c9086aadd5f8f73d22e373413bb557fe7e64
Betrix17 <beatamincic@gmail.com>
2026-04-10 10:40:47 +0200

    sere

---
+++ 3.3.6/app.js
@@ -0,0 +1,11 @@
+let counter = 0;
+
+let message = counter;
+
+while (counter <10) {
+    if (counter === 7) {
+        message = "7 je tady"
+    }
+    console.log(counter)
+    counter++;
+}
---
+++ Cviceni/script.js
@@ -0,0 +1,7 @@
+let age = 31;
+if (age>20)
+     {
+    console.log (`Věk je větší než 20. Tvůj věk je ${age}`);
+} else {
+    console.log("Věk je 20 nebo méně")
+}
